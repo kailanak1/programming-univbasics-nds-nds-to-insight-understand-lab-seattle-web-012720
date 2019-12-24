ea7b50578597da1087e3167cb2cd92011dbdeb89
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-24 20:10:06 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 
 def print_first_directors_movie_titles
   i = 0 
-  while i < directors_database[0][:movies].length do 
+  while i < directors_database[0].length do 
     print "\n#{directors_database[0][:movies][i][:title]}"
     i += 1 
   end
